5fb82b52eea4acad4c5b0ae566b8326fb6747e6f
Torogavrus <vvvas22@gmail.com>
2017-08-05 14:23:17 +0300

    simple add user form

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -1,4 +1,4 @@
-# from app import CONN
+from collections import OrderedDict
 from base_servise import BaseService
 # import MySQLdb
 #
@@ -14,6 +14,8 @@ from base_servise import BaseService
 # )
 #
 USERS_SELECT = 'SELECT * FROM user'
+SELECT_BY_NAME = 'SELECT * FROM user WHERE name="{}"'
+INSERT_USER = 'INSERT INTO user ({}) VALUES ({});'
 
 
 class UserService(BaseService):
@@ -25,6 +27,7 @@ class UserService(BaseService):
         if select_result:
             for user in select_result:
                 users.append({
+                    'id': user[0],
                     'name': user[1],
                     'email': user[2],
                     'status': self.get_status(user[3])
@@ -32,8 +35,48 @@ class UserService(BaseService):
 
         return users
 
+    def select_by_name(self, user_name):
+        print 444444444, user_name
+        select_result = self.sql_execute(SELECT_BY_NAME.format(user_name), fetchone=True)
+        return select_result
+
+    def add_user(self, user_data):
+        print 3333333, user_data
+
+        exist_in_db = self.select_by_name(user_data['name'])
+        if exist_in_db:
+            return 'User with name {} already exist!'
+
+        user_json = self.repair_json(user_data)
+
+        # user_json = OrderedDict(user_data)
+        # print 55555555555, user_json
+        # user_json['status'] = 1 if user_data['status'] == 'Active' else 0
+        #
+        # print 41414141, user_json, 41414, [x for x in user_json.itervalues()]
+        #
+        user_fields = (', ').join([x for x in user_json.keys()])
+        user_values = (', ').join([x for x in user_json.itervalues()])
+        sql = INSERT_USER.format(user_fields, user_values)
+        print 5555555555, user_fields, 555555, user_values, 555, sql
+        select_result = self.sql_execute(sql, commit=True)
+
+        if select_result:
+            return 'User with name {} added'
+
+        return select_result
+
     def get_status(self, status_id):
         if not status_id:
             return 'Inactive'
         else:
             return 'Active'
+
+    def repair_json(self, user_data):
+        user_json = {x: '"' + user_data[x] + '"' for x in user_data if x}
+        if user_json['status'] == '"Active"':
+            user_json['status'] = '1'
+        else:
+            user_json['status'] = '0'
+
+        return OrderedDict(user_json)
--- app/views/user_views.py
+++ app/views/user_views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import request, render_template
 from app import app
 from app.service.user_service import UserService
 
@@ -9,20 +9,26 @@ def index():
 
 @app.route('/users')
 def users():
-    u_s = UserService()
-    users = u_s.select_all_users()
+    user_s = UserService()
+    users = user_s.select_all_users()
     print 222222222, users
     return render_template('users.html', users=users)
     # return 'hello world'
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginFrom()
-#     if form.validate_on_submit():
-#         flash('Login request for openID="' + form.openid.data +
-#               '", remember_me=' + str(form.rememeber_me.data))
-#
-#         return redirect('./index')
-#
-#     return render_template('login.html', title="Sign In", form=form,
-#                            providers=app.config['OPENID_PROVIDERS'])
+@app.route('/add_user/', methods=['GET', 'POST'])
+def add_user():
+    error = None
+    if request.method == 'POST':
+        if request.form:
+            user_s = UserService()
+            added = user_s.add_user(request.form)
+            if not added:
+                error = 'User not added'
+            elif isinstance(added, str):
+                error = added
+                if 'User' in error:
+                    error = error.format(request.form['name'])
+        else:
+            error = 'Enter data, please'
+
+    return render_template('add_user.html', error=error)
