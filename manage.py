a5051f23d6ebc3cdb2604fe24da5eec835611b90
Torogavrus <vvvas22@gmail.com>
2017-08-07 09:14:30 +0300

    requirements added

--- manage.py
+++ manage.py
@@ -3,9 +3,9 @@
 from app import app
 from flask_script import Manager
 
-from app.service.user_service import UserService
 
 manager = Manager(app)
+
 
 if __name__ == '__main__':
     manager.run()
